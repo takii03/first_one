3dc991afff18be14b782080130f02a95b5957d16
takii09 <singhsuryap128@gmail.com>
2026-01-01 02:25:58 +0530

    hello

---
+++ file_one.py
@@ -0,0 +1,36 @@
+# def sum_difference(n=2):
+#     sum_n = n * (n + 1) // 2
+
+#     square_of_sum = sum_n ** 2
+#     sum_of_squares = n * (n + 1) * (2 * n + 1) // 6
+#     difference = square_of_sum - sum_of_squares
+#     print(difference)
+
+
+# num = input("Enter a number : ")
+
+
+# if num.strip() == "":
+#     print("Difference:", sum_difference())
+# else:
+#     n = int(num)
+#     print("Difference:", sum_difference(n))
+
+
+
+# def factorial(n): 
+#     if n == 0:
+#         return 1    
+
+#     return n*factorial(n-1)
+
+# num1 = int(input("Enter a non-negative integer: "))
+# print("Factorial of", num1, "is:", factorial(num1))
+ 
+import math
+# a=math.pi*(4**2)*angle?360
+# print(a)
+
+a=[12,45,65]
+print(sum(a))
+print(math.sqrt(4))
